11d55dad484b8076da514b44624b51106b15ca72
Ismael G. Jimenez <igjrbm@gmail.com>
2026-01-05 11:36:47 +0100

    Commit de Control

---
+++ 02.- Ejemplos/000 - ProyectoBase.cpp
@@ -0,0 +1,11 @@
+// Incluir la biblioteca estándar para entrada y salida
+#include <iostream>
+
+// Función principal donde comienza la ejecución del programa
+int main() { 
+	// Imprimir el mensaje en la consola
+    std::cout << "¡Hola Mundo desde LuisLlamas.es!" << std::endl;
+
+	 // Indicar que el programa terminó correctamente
+    return 0;
+}
