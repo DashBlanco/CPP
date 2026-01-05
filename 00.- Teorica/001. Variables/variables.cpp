abaf535be7dbd78d4a4739740c2162c9a50df654
Ismael G. Jimenez <igjrbm@gmail.com>
2026-01-05 11:19:03 +0100

    Commit de Control

---
+++ 00.- Teorica/001. Variables/variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  std::cout << "Hola desde Termux!" << endl;
+
+  int numero = 5;
+  float decimal = 3.1415f;
+  double doble = 3.14157547898544;
+  char caracter = 'a';
+  string cadena = "Cadena de texto";
+  bool boorlean = true; // True = 1 | False = 0
+
+  return 0;
+}
